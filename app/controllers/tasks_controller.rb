caa0174df7cf9352ff6f4da5a71bb9e658fbdc83
Said Rasinlic <saidrasinlic.jr@gmail.com>
2023-02-14 23:38:10 +0100

    Add file

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,7 +9,7 @@ class TasksController < ApplicationController
 
     respond_to do |format|
       if @task.save
-        format.html { redirect_to tasks_url, notice: "Task was successfully created" }
+        format.html { redirect_to tasks_url, notice: 'Task was successfully created' }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
